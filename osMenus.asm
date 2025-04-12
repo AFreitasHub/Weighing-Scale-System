34bfe1e0e6bb37214bbf4dfddc65ac6b3d69dd61
Lourenço Nóbrega <lourenco.ferraz.nobrega@gmail.com>
2025-04-12 17:09:21 +0100

    Add files via upload

---
+++ osMenus.asm
@@ -0,0 +1,77 @@
+PLACE 4000H
+ProductsNames:
+		Int    100
+		String "Uvas            "
+		Int    534
+		Int    101
+		String "Melancia        "
+		Int    187
+		Int    102
+		String "Ananás          "
+		Int    187
+		Int    103
+		String "Kiwi            "
+		Int    356
+		Int    104
+		String "Pêssego         "
+		Int    446
+		Int    105
+		String "Banana          "
+		Int    258
+		Int    106
+		String "Morango         "
+		Int    446
+		Int    107
+		String "Framboesa       "
+		Int    1781
+		Int    108
+		String "Laranja         "
+		Int    160
+		Int    109
+		String "Tangerina       "
+		Int    222
+		Int    110
+		String "Cenoura         "
+		Int    104
+		Int    111
+		String "Batata          "
+		Int    114
+		Int    112
+		String "Nabo            "
+		Int    228
+		Int    113
+		String "Beterraba       "
+		Int    523
+		Int    114
+		String "Alho            "
+		Int    619
+		Int    115
+		String "Cebola          "
+		Int    143
+		Int    116
+		String "Ervilha         "
+		Int    142
+		Int    117
+		String "Lentilhas       "
+		Int    219
+		Int    118
+		String "Trigo           "
+		Int    95
+		Int    119
+		String "Milho           "
+		Int    362
+		Int    120
+		String "Favas           "
+		Int    407
+		Int    121
+		String "Castanhas       "
+		Int    892
+		Int    122
+		String "Noz             "
+		Int    1839
+		Int    123
+		String "Amendoim        "
+		Int    803
+		Int    124
+		String "Café            "
+		Int    2025
